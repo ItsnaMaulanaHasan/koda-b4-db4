a3e560e62bd85812ed600c365f3d6f3fab94d5ab
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-29 22:04:44 +0700

    feat: query for get most productive director and most bussy year

--- query3.sql
+++ query3.sql
@@ -19,3 +19,38 @@ GROUP BY
     actors.last_name
 HAVING
     COUNT(roles.actor_id) > 5;
+
+-- Mendapatkan directors paling produktif sepanjang masa
+SELECT directors.first_name, directors.last_name, COUNT(movies_directors.director_id)
+FROM directors
+    JOIN movies_directors ON directors.id = movies_directors.director_id
+GROUP BY
+    directors.id,
+    directors.first_name,
+    directors.last_name
+HAVING
+    COUNT(movies_directors.director_id) = (
+        SELECT MAX(movie_count)
+        FROM (
+                SELECT COUNT(movies_directors.movie_id) AS movie_count
+                FROM movies_directors
+                GROUP BY
+                    movies_directors.director_id
+            )
+    );
+
+-- -- Mendapatkan tahun tersibuk sepanjang masa
+SELECT year, COUNT(year) AS total_movie
+FROM movies
+GROUP BY
+    year
+HAVING
+    COUNT(year) = (
+        SELECT MAX(count_year)
+        FROM (
+                SELECT COUNT(year) AS count_year
+                FROM movies
+                GROUP BY
+                    year
+            )
+    );
