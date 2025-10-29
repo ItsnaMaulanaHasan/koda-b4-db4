b551284579307cf03fc10da5e10c69bd67bb2ce3
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-29 21:41:35 +0700

    feat: adjust query task no 1 and 2

--- query3.sql
+++ query3.sql
@@ -1,15 +1,21 @@
 -- Mendapatkan director, dan berapa genre yang di direct
-SELECT directors.first_name, directors.last_name, COUNT(directors_genres.director_id) as total_genre
+SELECT directors.first_name, directors.last_name, COUNT(
+        DISTINCT directors_genres.genre
+    ) as total_genre
 FROM directors
     JOIN directors_genres ON directors.id = directors_genres.director_id
 GROUP BY
-    directors.id;
+    directors.id,
+    directors.first_name,
+    directors.last_name;
 
 -- Mendapatkan actors yang memiliki roles lebih dari 5
 SELECT actors.first_name, actors.last_name, COUNT(roles.actor_id) AS total_roles
 FROM actors
     JOIN roles ON actors.id = roles.actor_id
 GROUP BY
-    actors.id
+    actors.id,
+    actors.first_name,
+    actors.last_name
 HAVING
-    COUNT(roles) > 5;
+    COUNT(roles.actor_id) > 5;
