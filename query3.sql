290f8b0f43b8cbb440bfde2ca663b3b0e924eb65
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-29 17:23:37 +0700

    feat: query for get total roles per actors

--- query3.sql
+++ query3.sql
@@ -1,5 +1,5 @@
 -- Mendapatkan director, dan berapa genre yang di direct
-SELECT directors.first_name, directors.last_name, count(directors_genres.genre) as total_genre
+SELECT directors.first_name, directors.last_name, count(directors_genres.director_id) as total_genre
 FROM directors
     JOIN directors_genres ON directors.id = directors_genres.director_id
 GROUP BY (
@@ -7,3 +7,12 @@ GROUP BY (
         directors.first_name,
         directors.last_name
     );
+
+-- Mendapatkan actors yang memiliki roles lebih dari 5
+SELECT actors.first_name, actors.last_name, count(roles.actor_id) as total_roles
+FROM actors
+    JOIN roles ON actors.id = roles.actor_id
+GROUP BY
+    actors.id
+HAVING
+    count(roles) > 5;
