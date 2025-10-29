76343aa88ab3a7b081eee6991385e52e8da7fd6b
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-29 17:46:54 +0700

    refactor: adjust query

--- query3.sql
+++ query3.sql
@@ -1,15 +1,15 @@
 -- Mendapatkan director, dan berapa genre yang di direct
-SELECT directors.first_name, directors.last_name, count(directors_genres.director_id) as total_genre
+SELECT directors.first_name, directors.last_name, COUNT(directors_genres.director_id) as total_genre
 FROM directors
     JOIN directors_genres ON directors.id = directors_genres.director_id
 GROUP BY
-    directors.id
+    directors.id;
 
 -- Mendapatkan actors yang memiliki roles lebih dari 5
-SELECT actors.first_name, actors.last_name, count(roles.actor_id) as total_roles
+SELECT actors.first_name, actors.last_name, COUNT(roles.actor_id) AS total_roles
 FROM actors
     JOIN roles ON actors.id = roles.actor_id
 GROUP BY
     actors.id
 HAVING
-    count(roles) > 5;
+    COUNT(roles) > 5;
